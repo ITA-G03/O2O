deab649f68580871dd09fd5b16e901c042f47b3d
ZHANGJA4 <JAMES.ZHANG@OOCL.COM>
2015-08-25 20:18:56 +0800

    update BusinessController

--- BUSINESS/src/ita/o2o/controller/restful/BusinessRestController.java
+++ BUSINESS/src/ita/o2o/controller/restful/BusinessRestController.java
@@ -6,24 +6,40 @@ import ita.o2o.entity.extra.Status;
 import ita.o2o.entity.location.Area;
 import ita.o2o.entity.location.City;
 import ita.o2o.entity.location.Location;
-import ita.o2o.util.bean.ResponseMessage;
+
+import ita.o2o.util.mapper.JSONMapper;
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.LinkedList;
+import java.util.List;
+
+
 @Controller
+@RequestMapping(value = "/business")
 public class BusinessRestController {
+    @Autowired
+    JSONMapper jsonMapper;
+
+
+    private static Logger logger = Logger.getLogger(BusinessRestController.class);
 
     /**
      * 改变对应商家的信息
      *
-     * @param business
+     * @param
      * @return
      */
     @ResponseBody
-    @RequestMapping(method = RequestMethod.POST, value = "/business/change")
-    public void changeBusinessInfo(@RequestParam("signboard") String name
-                                   ) {
+    @RequestMapping(method = RequestMethod.POST, value = "/change")
+    public void changeBusinessInfo(@RequestParam("signboard") String name, @RequestParam("license") MultipartFile file
+    ) {
+        logger.info("filesize: " + file.getSize());
+        logger.info("OriginalFilename: " + file.getOriginalFilename());
+        logger.info("Name:" + file.getName());
 //        System.out.println(business);
         return;
     }
@@ -36,7 +52,7 @@ public class BusinessRestController {
      * @return
      */
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET, value = "/business/setting/profile")
+    @RequestMapping(method = RequestMethod.GET, value = "/setting/profile")
     public Business getCurrentBusinessInfo() {
 //        System.out.printf(businessId.toString());
 //        System.out.printf(businessId.toString());
@@ -66,6 +82,22 @@ public class BusinessRestController {
         return business;
     }
 
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
+    public String getALlBusiness() {
+        List<Business> businessList = new LinkedList<Business>();
+        Business b1 = new Business();
+        b1.setBusinessId(0);
+        b1.setRealName("掌机");
+        Business b2 = new Business();
+        b2.setBusinessId(1);
+        b2.setRealName("啦啦");
+        businessList.add(b1);
+        businessList.add(b2);
+        return jsonMapper.writeObjectAsDataString(businessList);
+    }
+
     /**
      * 获得对应商家的信息
      *
@@ -73,7 +105,7 @@ public class BusinessRestController {
      * @return
      */
     @ResponseBody
-    @RequestMapping(method = RequestMethod.GET, value = "/business/{businessId}")
+    @RequestMapping(method = RequestMethod.GET, value = "/{businessId}")
     public Business getBusinessInfo(@PathVariable Integer businessId) {
         System.out.printf(businessId.toString());
         Business business = new Business();
