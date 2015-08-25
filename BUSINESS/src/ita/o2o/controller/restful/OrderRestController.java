5ade8c995d645f4abb1a1fbcbd2356b00a0b80d1
ZHANGJA4 <JAMES.ZHANG@OOCL.COM>
2015-08-25 20:23:37 +0800

    update Order reference

---
+++ BUSINESS/src/ita/o2o/controller/restful/OrderRestController.java
@@ -0,0 +1,12 @@
+package ita.o2o.controller.restful;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by ZHANGJA4 on 8/25/2015.
+ */
+@Controller
+//@RequestMapping(produces = "application/json;charest=utf-8")
+public class OrderRestController {
+}
