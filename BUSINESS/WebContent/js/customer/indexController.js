94eea9b7f882e209fe0faa14ae649e0c2890d6b5
kary guo <kary.guo@oocl.com>
2015-08-26 17:23:23 +0800

    front end md5

--- BUSINESS/WebContent/js/customer/indexController.js
+++ BUSINESS/WebContent/js/customer/indexController.js
@@ -1,9 +1,15 @@
-app.controller('locationCtrl', function ($scope, $modal, $http) {
+app.controller('locationCtrl', function ($scope, $modal, $http, $cookieStore) {
 
     $http.get('/rest/location/city').success(function (data) {
         $scope.cities = data;
     })
 
+    $scope.history = $cookieStore.get('locations');
+    $scope.searchFromHistory = function () {
+        var form = $('form.change-location-form');
+        form.find('select[name=cityId]').val($scope.history[$scope.historyIndex].city.cityId);
+        form.find('input[name=areaName]').val($scope.history[$scope.historyIndex].area.areaName);
+    }
 })
 
 $(function () {
