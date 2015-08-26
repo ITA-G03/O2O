f5ab1d88797c3f2a988bcb31a1e71182cf351e71
Jason Cui <Jason.Cui@oocl.com>
2015-08-26 11:13:30 +0800

    Update Constants

--- BUSINESS/src/ita/o2o/constants/O2OConstants.java
+++ BUSINESS/src/ita/o2o/constants/O2OConstants.java
@@ -15,4 +15,24 @@ public class O2OConstants {
     public static final int BUSINESS_STATUS_REJECT = 3;
     public static final int BUSINESS_STATUS_HOT = 4;
 
+
+
+
+    /*常见返回值定义*/
+    public static final int UPLOAD_FAILURE=-1;
+
+    public static final int LOGIN_SUCCESS=1;
+    public static final int LOGIN_USER_NOT_EXIST = 2;
+    public static final int LOGIN_PASSWORD_WRONG=3;
+    public static final int LOGIN_USER_APPROVING=4;
+
+
+
+    public static final int DEFAULT_FAILURE_RESULT=0;
+    public static final int DEFAULT_FAILURE_CODE=-1;
+
+
+
+
+
 }
